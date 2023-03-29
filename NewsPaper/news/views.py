4d9b9b8b0fae9835187b2531e75beed1a2f8d0a4
SerjMarkby <serj.markby@gmail.com>
2023-03-30 02:07:05 +0300

    Добавил представления на поиск, добавление, удаление новостей

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from .forms import PostForm
+from .filters import PostFilter
 
 # Create your views here.
 
@@ -10,9 +13,58 @@ class PostList(ListView):
     ordering = '-pk'
     template_name = 'posts.html'
     context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = PostFilter(self.request.GET, queryset)
+        return self.filterset.qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["filterset"] = self.filterset
+        return context
+
+
+class SearchPosts(ListView):
+    model = Post
+    template_name = 'search.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_filter(self):
+        return PostFilter(self.request.GET, queryset=super().get_queryset())
+
+    def get_queryset(self):
+        return self.get_filter().qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = PostFilter(self.request.GET,
+                                       queryset=self.get_queryset())  # вписываем наш фильтр в контекст
+        context['form'] = PostFilter()
+        return context
 
 
 class PostDetail(DetailView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
+
+
+class PostCrate(CreateView):
+    form_class = PostForm
+    model = Post
+    template_name = "post_edit.html"
+
+
+class PostUpdate(UpdateView):
+    form_class = PostForm
+    model = Post
+    template_name = "post_edit.html"
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("posts")
