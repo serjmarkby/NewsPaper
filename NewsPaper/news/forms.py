86256ed6e1fbb4752082e8afe6ea36e464362f70
SerjMarkby <serj.markby@gmail.com>
2023-03-30 02:32:08 +0300

    Добавил форму для статей

--- NewsPaper/news/forms.py
+++ NewsPaper/news/forms.py
@@ -36,3 +36,38 @@ class PostForm(forms.ModelForm):
                 "Заголовок и текст должны начинаться с заглавной буквы"
             )
         return cleaned_data
+
+
+class ArticlForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            "author",
+            "title",
+            "img",
+            "text",
+            "category",
+        ]
+
+        widgets = {
+            "author": forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'userid'}),
+            "title": forms.TextInput(attrs={'class': 'form-control'}),
+            # "img": forms.FileInput()
+            "text": forms.Textarea(attrs={'class': 'form-control'}),
+            # "category": forms.ChoiceField(self, to_field_name="category"),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        title = cleaned_data.get("title")
+        text = cleaned_data.get("text")
+
+        if len(title) < 24 or len(text) < 512:
+            raise ValidationError(
+                "Заголовок должен содержать минимум 24 символа, текст минимум 512 символа"
+            )
+        elif title[0].islower() or text[0].islower():
+            raise ValidationError(
+                "Заголовок и текст должны начинаться с заглавной буквы"
+            )
+        return cleaned_data
