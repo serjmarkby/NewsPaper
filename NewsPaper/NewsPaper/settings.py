6ef0f7b77a1aff3c4c7bcf2616920a5999943287
SerjMarkby <serj.markby@gmail.com>
2023-03-30 02:06:25 +0300

    Не сохранялись картинки, поправил MEDIA_ROOT

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
+    'django_filters',
 ]
 
 SITE_ID = 1
@@ -124,6 +125,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
