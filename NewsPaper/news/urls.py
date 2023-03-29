708bed3f05a7769f9a7ad52190caad0c7f7ae3a3
SerjMarkby <serj.markby@gmail.com>
2023-03-30 02:05:59 +0300

    Добавил новые пути для апдейтов, добавления, удаления новостей

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, PostCrate, PostUpdate, PostDelete, SearchPosts
 
 
 urlpatterns = [
-   path('', PostList.as_view()),
-   path('<int:pk>', PostDetail.as_view()),
+   path('', PostList.as_view(), name="posts"),
+   path('<int:pk>', PostDetail.as_view(), name="post"),
+   path('create', PostCrate.as_view(), name="post_edit"),
+   path('<int:pk>/update', PostUpdate.as_view(), name="post_update"),
+   path('<int:pk>/delete', PostDelete.as_view(), name="post_delete"),
+   path('search', SearchPosts.as_view(), name='search'),
 ]
